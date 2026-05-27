141ec90a1e0fa6b377d772ab26bacc5abc65379a
Zaidibeth Ramos <zaidi.ramos@gmail.com>
2026-05-26 21:23:00 -0400

    feat: add Instrument Serif font and update weight configurations for existing fonts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,14 +1,22 @@
-import { Geist, JetBrains_Mono } from "next/font/google";
+import { Geist, JetBrains_Mono, Instrument_Serif } from "next/font/google";
 
-export const geistSans = Geist({
-  variable: "--font-geist-sans",
+export const instrumentSerif = Instrument_Serif({
+  variable: "--font-instrument-serif",
   subsets: ["latin"],
+  weight: "400"
 });
 
 export const jetBrainsMono = JetBrains_Mono({
   variable: "--font-jetbrains-mono",
   subsets: ["latin"],
+  weight: ["400", "500"],
+});
+
+export const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600"],
 });
 
 /** Space-separated CSS variable classes for `<body>`. */
-export const fontVariables = `${geistSans.variable} ${jetBrainsMono.variable}`;
+export const fontVariables = `${instrumentSerif.variable} ${jetBrainsMono.variable} ${geistSans.variable}`;
